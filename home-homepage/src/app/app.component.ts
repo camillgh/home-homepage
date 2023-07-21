25dd2dccc12e8f583dd577a31bf68759a5248b58
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-07-21 15:24:32 +0200

    🔥 Remove old app.component routing and navigation

--- home-homepage/src/app/app.component.ts
+++ home-homepage/src/app/app.component.ts
@@ -1,41 +1,10 @@
-import { Component, HostListener } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  activeRoute: string = '/';
-
-  constructor(private router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.activeRoute = event.url;
-      }
-    });
-  }
-
-  // Add a scroll event listener to update the activeRoute when scrolling
-  @HostListener('window:scroll', ['$event'])
-  onScroll() {
-    const currentRoute = this.router.url;
-    const landingPageContainer = document.querySelector('.planting-page') as HTMLElement;
-    const componentContainers = document.querySelectorAll('.component-container');
-
-    if (landingPageContainer) {
-      const landingPageTop = landingPageContainer.getBoundingClientRect().top;
-      if (landingPageTop < 0) {
-        // Check if any component container is in view and update activeRoute accordingly
-        componentContainers.forEach((container: Element) => {
-          const componentTop = (container as HTMLElement).offsetTop;
-          const componentBottom = componentTop + (container as HTMLElement).offsetHeight;
-          if (window.pageYOffset >= componentTop && window.pageYOffset <= componentBottom) {
-            this.activeRoute = currentRoute;
-          }
-        });
-      }
-    }
-  }
+  title = 'home-homepage';
 }
