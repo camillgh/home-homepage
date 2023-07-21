bf66d7960d4f70a12e6e0e1c746d95903a2ade24
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-07-21 15:25:09 +0200

    🚨 🚚 Rename app.module to planting and format

--- home-homepage/src/app/app.module.ts
+++ home-homepage/src/app/app.module.ts
@@ -3,22 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './planting-page/planting-page.component';
+import { PlantingPageComponent } from './planting-page/planting-page.component';
 import { KokebokPageComponent } from './kokebok-page/kokebok-page.component';
 import { ToDoodelidooPageComponent } from './to-doodelidoo-page/to-doodelidoo-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
+    PlantingPageComponent,
     KokebokPageComponent,
-    ToDoodelidooPageComponent
+    ToDoodelidooPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
