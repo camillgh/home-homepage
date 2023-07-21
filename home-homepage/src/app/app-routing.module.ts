c73bec0fc326b4b2fdc983fe3e89805591522346
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-07-21 15:23:51 +0200

    🚚  Rename routing module to planting

--- home-homepage/src/app/app-routing.module.ts
+++ home-homepage/src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Import your components here
-import { LandingPageComponent } from './planting-page/planting-page.component';
+import { PlantingPageComponent } from './planting-page/planting-page.component';
 import { KokebokPageComponent } from './kokebok-page/kokebok-page.component';
 import { ToDoodelidooPageComponent } from './to-doodelidoo-page/to-doodelidoo-page.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent },
+  { path: 'planteting', component: PlantingPageComponent },
   { path: 'kokebok', component: KokebokPageComponent },
   { path: 'to-doodelidoo', component: ToDoodelidooPageComponent },
-  { path: '**', redirectTo: '' }, // Redirect any other route to the landing page
+  { path: '**', redirectTo: '' }, // Redirect any other route to the Planting page
 ];
 
 @NgModule({
