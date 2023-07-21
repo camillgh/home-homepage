e2d4864a0a4ebc134f083f40f1cc4b77c47040e0
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-07-21 16:39:19 +0200

    ✨  Add landing page component

--- home-homepage/src/app/app-routing.module.ts
+++ home-homepage/src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { PlantingPageComponent } from './planting-page/planting-page.component';
 import { KokebokPageComponent } from './kokebok-page/kokebok-page.component';
 import { ToDoodelidooPageComponent } from './to-doodelidoo-page/to-doodelidoo-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: 'planteting', component: PlantingPageComponent },
-  { path: 'kokebok', component: KokebokPageComponent },
-  { path: 'to-doodelidoo', component: ToDoodelidooPageComponent },
-  { path: '**', redirectTo: '' }, // Redirect any other route to the Planting page
+  {
+    path: '',
+    component: LandingPageComponent,
+    children: [
+      { path: 'planteting', component: PlantingPageComponent },
+      { path: 'kokebok', component: KokebokPageComponent },
+      { path: 'to-doodelidoo', component: ToDoodelidooPageComponent },
+    ],
+  },
 ];
 
 @NgModule({
