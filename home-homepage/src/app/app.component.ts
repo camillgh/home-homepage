7c8b4b61e4461a7bbed5fbb293a6989e531ab0ec
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-07-21 10:46:22 +0200

    ✨ Add active feature to toolbar items

--- home-homepage/src/app/app.component.ts
+++ home-homepage/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'home-homepage';
+  activeRoute: string = '/';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.activeRoute = event.url;
+      }
+    });
+  }
+
+  // Add a scroll event listener to update the activeRoute when scrolling
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const currentRoute = this.router.url;
+    const landingPageContainer = document.querySelector('.landing-page') as HTMLElement;
+    const componentContainers = document.querySelectorAll('.component-container');
+
+    if (landingPageContainer) {
+      const landingPageTop = landingPageContainer.getBoundingClientRect().top;
+      if (landingPageTop < 0) {
+        // Check if any component container is in view and update activeRoute accordingly
+        componentContainers.forEach((container: Element) => {
+          const componentTop = (container as HTMLElement).offsetTop;
+          const componentBottom = componentTop + (container as HTMLElement).offsetHeight;
+          if (window.pageYOffset >= componentTop && window.pageYOffset <= componentBottom) {
+            this.activeRoute = currentRoute;
+          }
+        });
+      }
+    }
+  }
 }
