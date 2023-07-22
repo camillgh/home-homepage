8989c81eacfe5bc78e70af2107f8ab5438c14e86
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-07-22 13:21:00 +0200

    🚨  Lint planting-page.component.ts

--- home-homepage/src/app/planting-page/planting-page.component.ts
+++ home-homepage/src/app/planting-page/planting-page.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-planting-page',
   templateUrl: './planting-page.component.html',
-  styleUrls: ['./planting-page.component.scss']
+  styleUrls: ['./planting-page.component.scss'],
 })
 export class PlantingPageComponent {}
-
-}
