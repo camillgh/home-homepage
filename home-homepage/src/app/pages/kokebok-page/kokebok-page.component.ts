6af560c9e37104bd6bb97ae5fa8bc57a39bacdbc
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-07-26 23:03:34 +0200

    💩 Add shitty first page turning animation

--- home-homepage/src/app/pages/kokebok-page/kokebok-page.component.ts
+++ home-homepage/src/app/pages/kokebok-page/kokebok-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-kokebok-page',
   templateUrl: './kokebok-page.component.html',
-  styleUrls: ['./kokebok-page.component.scss']
+  styleUrls: ['./kokebok-page.component.scss'],
 })
-export class KokebokPageComponent {
+export class KokebokPageComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const pages = document.getElementsByClassName('page');
+    const pageNumbers = Array.from(pages).map((_, i) => i + 1);
 
+    for (let i = 0; i < pages.length; i++) {
+      const page = pages[i] as HTMLElement;
+
+      if (i % 2 === 0) {
+        page.style.zIndex = (pages.length - i).toString();
+      }
+
+      // Add the click event handler to each page element
+      page.addEventListener('click', function () {
+        const pageNum = pageNumbers[i];
+
+        if (pageNum % 2 === 0) {
+          this.classList.remove('flipped');
+          const prevElement = this.previousElementSibling as HTMLElement;
+          prevElement?.classList.remove('flipped');
+        } else {
+          this.classList.add('flipped');
+          const nextElement = this.nextElementSibling as HTMLElement;
+          nextElement?.classList.add('flipped');
+        }
+      });
+    }
+  }
 }
