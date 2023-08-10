b8478bd99c589f3c142c63bfd38ef0d865ee9d95
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-08-10 17:34:27 +0200

    Add simple edit and drag and drop plant

--- home-homepage/src/app/pages/planting-page/planting-page.component.ts
+++ home-homepage/src/app/pages/planting-page/planting-page.component.ts
@@ -5,4 +5,65 @@ import { Component, HostListener } from '@angular/core';
   templateUrl: './planting-page.component.html',
   styleUrls: ['./planting-page.component.scss'],
 })
-export class PlantingPageComponent {}
+export class PlantingPageComponent {
+  plants = [
+    {
+      name: 'Plant A',
+      image: 'assets/lavender.png',
+      top: 30,
+      left: 40,
+      humidity: 65,
+      healthy: true,
+    },
+    // Add more plant objects as needed
+  ];
+
+  editMode = false;
+  dragging = false;
+  activePlantIndex: number | null = null;
+  initialMouseX = 0;
+  initialMouseY = 0;
+
+  toggleEditMode() {
+    if (!this.editMode) {
+      this.activePlantIndex = null;
+    }
+    this.editMode = !this.editMode;
+  }
+
+  startDrag(event: MouseEvent, index: number) {
+    if (this.editMode) {
+      this.dragging = true;
+      this.activePlantIndex = index;
+      this.initialMouseX = event.clientX;
+      this.initialMouseY = event.clientY;
+    }
+  }
+
+  @HostListener('document:mouseup')
+  endDrag() {
+    this.dragging = false;
+    this.activePlantIndex = null;
+    this.savePlantPositions();
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  drag(event: MouseEvent) {
+    if (this.dragging && this.activePlantIndex !== null) {
+      const deltaX = event.clientX - this.initialMouseX;
+      const deltaY = event.clientY - this.initialMouseY;
+
+      this.plants[this.activePlantIndex].top +=
+        (deltaY * 100) / window.innerHeight;
+      this.plants[this.activePlantIndex].left +=
+        (deltaX * 100) / window.innerWidth;
+
+      this.initialMouseX = event.clientX;
+      this.initialMouseY = event.clientY;
+    }
+  }
+
+  private savePlantPositions() {
+    // Implement your logic to save the updated plant positions to a data store or backend.
+  }
+}
