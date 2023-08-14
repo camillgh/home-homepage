d04b052c548f9f870de218859e8cf3d4e07f042b
Camilla Marie Greve Hartviksen <millagreveh@gmail.com>
2023-08-14 21:38:06 +0200

    🐛 Add editsaveveggbutton component to module

--- home-homepage/src/app/app.module.ts
+++ home-homepage/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { KokebokPageComponent } from './pages/kokebok-page/kokebok-page.componen
 import { ToDoodelidooPageComponent } from './pages/to-doodelidoo-page/to-doodelidoo-page.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { HomeButtonComponent } from './buttons/home-button/home-button.component';
+import { EditSaveVeggButtonComponent } from './buttons/edit-save-vegg-button/edit-save-vegg-button.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { HomeButtonComponent } from './buttons/home-button/home-button.component
     ToDoodelidooPageComponent,
     LandingPageComponent,
     HomeButtonComponent,
+    EditSaveVeggButtonComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
